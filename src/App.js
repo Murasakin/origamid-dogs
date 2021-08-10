54dbcd566727de376c4c5b38504c22cae4815175
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2021-08-10 09:39:18 -0300

    11 validate token

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 import './App.css';
 
@@ -10,12 +11,14 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
