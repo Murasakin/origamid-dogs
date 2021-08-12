f69a32739d262c7ee5821c974f7c356a13929453
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2021-08-12 14:40:40 -0300

    21 feed modal 2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div className={styles.ooter}>Footer inicial</div>;
+  return <div className={styles.footer}></div>;
 };
 
 export default Footer;
